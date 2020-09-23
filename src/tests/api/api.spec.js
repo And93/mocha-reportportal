67933afa6345b93a15e1a9637934cd842620da3b
Andrei D <andreidei4ik@gmail.com>
2020-09-23 09:18:59 +0300

    Update api.spec.js

--- src/tests/api/api.spec.js
+++ src/tests/api/api.spec.js
@@ -6,7 +6,7 @@ const {execCmd} = require('../../helpers/childProcessesHelper');
 
 describe('API', () => {
     it('Test', async () => {
-        const text = await execCmd('echo "Hi M&U team!"');
-        expect(text).to.be.contains('Hi M&U team!');
+        const text = await execCmd('echo "Hi team!"');
+        expect(text).to.be.contains('Hi team!');
     });
 });
